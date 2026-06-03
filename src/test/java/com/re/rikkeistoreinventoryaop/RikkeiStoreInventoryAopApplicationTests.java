c841c7abba2a23087f2e2d7a4e8d94f9431067d8
breakBlogHubVercel <breakgamer2601@gmail.com>
2026-06-03 08:52:15 +0700

    up

--- src/test/java/com/re/rikkeistoreinventoryaop/RikkeiStoreInventoryAopApplicationTests.java
+++ src/test/java/com/re/rikkeistoreinventoryaop/RikkeiStoreInventoryAopApplicationTests.java
@@ -5,6 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {
         "spring.datasource.url=jdbc:h2:mem:context-test",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
         "spring.jpa.hibernate.ddl-auto=create-drop"
 })
 class RikkeiStoreInventoryAopApplicationTests {
